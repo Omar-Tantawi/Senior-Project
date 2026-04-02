e9932f83e42b3165a00216f2d1b050c0e2de8d11
Omar-Tantawi <2002omartantawi@gmail.com>
2026-04-02 15:52:44 +0300

    Initial Laravel backend setup

---
+++ backend/routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+Route::get('/students', [StudentController::class, 'index']);
+Route::get('/test', function() {
+    return ['status' => 'ok'];
+});
